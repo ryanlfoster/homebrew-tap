2d754146f088e90dcc8caf89ff23745db14da9b2
joshh <joshua_hansen@yahoo.com>
2014-04-12 10:20:42 -0700

    update caveats

--- cq-tools.rb
+++ cq-tools.rb
@@ -46,29 +46,16 @@ class CqTools < Formula
 
     cq-tools configuration files were created.
 
-    You *MUST* configure the system before using these tools. As well, Java 7 is required for cq-sync.
+    You *MUST* configure the system before using these tools.
 
-    There are two ways to configure these tools.
+    Note: Java 7 is required for cq-sync.
 
-    1. Simple - if you only have one project and one CQ instance use this
-    2. Switchable - if you have multiple projects or switch between multiple CQ servers use this
-
-    ### Simple
-
-    1. Edit ~/.cq-env to match your system
-
-    **CQ_SERVER_HOME** should point to the root of your server (e.g. ~/Servers/cq5/cq-quickstart)
-
-    **CQ_BRANCH_HOME** should point to your CQ project and is only necessary to be set if you use cq-sync - this will sync all files realtime found under /jcr_root/
-
-    ### Switchable
-
-    1. Edit ~/.cq-cfg to your requirements, adding as many servers and/or branches as required
-    2. Run: cq-set-server and choose the server you want to use for the current session
-    3. Run: cq-set-branch and choose the branch you want to use for the current session
+    1. Add 'source ~/.cq/env' to ~/.bash_profile
+    2. Edit ~/.cq-cfg to your requirements, adding as many servers and/or branches as required
+    3. Run: cq-set-server and choose the server you want to use for the current session
+    4. Run: cq-set-branch and choose the branch you want to use for the current session
 
     These scripts effectively manage the .cq-env file for you based on your configuration settings.
-
 
     ### More information
     You can read more on the cq-tools here: https://github.com/joshes/cq-tools
