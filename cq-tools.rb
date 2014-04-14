fcc267ac07aeda1608f8ca91a02a1e73565db91b
joshh <joshua_hansen@yahoo.com>
2014-04-14 13:30:42 -0700

    bump version

--- cq-tools.rb
+++ cq-tools.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 
 class CqTools < Formula
 
-  version '1.0'
+  version '1.1'
   homepage 'https://github.com/joshes/cq-tools'
   url 'https://github.com/joshes/directory-watcher/releases/download/directory-watcher-1.1/DirectoryWatcher.jar'
   sha1 '0e804fb61ffd22c80ec39e68b8d03167b32d2c22'
